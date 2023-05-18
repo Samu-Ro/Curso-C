cf9925fbd9679e3287c439c82a35d584c10296fc
Samu-Ro <129281088+Samu-Ro@users.noreply.github.com>
2023-05-18 12:01:28 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+int main() {
+    //Se declara una variable de nombre "num1" de tipo entero (int) y se le asigna el valor 1
+    int num1 = 1;
+
+   //Imprime en pantalla el valor de la variable "num1"
+    printf("El valor de 'num1' (int) es %d\n", num1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //Se declara una variable de nombre "num2" de tipo float  y se le asigna el valor 3.54321
+    float num2 = 3.54321;
+
+   //Imprime en pantalla el valor de la variable "num2"
+    printf("El valor de 'num2' (float) es %.2f\n", num2);
+
+
+
+
+
+
+
+
+//Se declara una variable de nombre "caracteer" de tipo caracter (char)  y se le asigna el valor 'a'
+    char caracter = 'a';
+
+   //Imprime en pantalla el valor de la variable "caracter"
+    printf("El valor de 'caracter' (char) es %c\n", caracter);
+
+
+
+
+
+    //getchar Espera que el usuario ingrese un valor por teclado
+    getchar ();
+    return 0;
+}
+
+
+
+
+
+
+
+
+
